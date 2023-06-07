c70dc721e7a5083cd13b4c509ea88b5f5a548afd
Rutembeza Mugisha Fabrice <130748178+Fabrice-G@users.noreply.github.com>
2023-06-07 13:35:54 +0200

    just uncomment
    test

--- Aufgabe.py
+++ Aufgabe.py
@@ -1,15 +1,15 @@
-# input_string = input ('Enter the string: ')
-# print(input_string[0:-2])
+input_string = input ('Enter the string: ')
+print(input_string[0:-2])
 
-# def undersorer (): 
-#    input_string1 = input ('Enter the string: ')
-#    out1= '_'
-#    out2= ""
-#    for chr in input_string1:
-#        out2 += chr + out1
-#    print (out2)
+def undersorer (): 
+   input_string1 = input ('Enter the string: ')
+   out1= '_'
+   out2= ""
+   for chr in input_string1:
+       out2 += chr + out1
+   print (out2)
         
-# undersorer() 
+undersorer()  
 
 def remove(string):
     return string.replace(" ", "")
